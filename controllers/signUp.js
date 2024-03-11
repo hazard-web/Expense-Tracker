a17dee4e2346810ae0ca31fe201f478fc1c3d12b
Shivam Bhardwaj <shivambhardwaj403@gmail.com>
2024-03-11 18:13:02 +0530

    "Add user registration functionality to the application
    Implement postSignup to handle user registration requests
    Hash the user's password using bcrypt before storing it in the database
    Create a new user and return a success message upon successful registration"

--- controllers/signUp.js
+++ controllers/signUp.js
@@ -1,24 +1,28 @@
 const User = require('../models/user');
 const path = require('path');
+const bcrypt = require('bcrypt');
 
 exports.getUser = async (req, res, next) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'views', 'signUp.html'))
 };
 
-exports.postUser = async (req,res,next) => {
-    const {name, email, password} = req.body;
-    try{
+
+exports.postUser = async (req, res, next) => {
+    const { name, email, password } = req.body;
+    try {
+        // Hash the password
+        const hashedPassword = await bcrypt.hash(password, 10);
+        
+        // Create the user with the hashed password
         const user = await User.create({
             name,
             email,
-            password
+            password: hashedPassword // Store hashed password in the database
         });
+
         res.status(200).json(user);
-    } catch (err) {
-        console.error(err);
-
-        res.status(500).json({
-            error: 'Error creating the user'})
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Error creating the user' });
     }
-
-}
+};
