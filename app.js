c41e992fc55493e2bc7ff02fb0b9f9e052285328
Shivam Bhardwaj <shivambhardwaj403@gmail.com>
2024-06-12 15:02:15 +0530

    "Refactored app.js, added resetPasswordRouter, removed try-catch block, updated userController.js, package-lock.json, package.json, and public views/files"Sending Reset Password Link and Update the Password

--- app.js
+++ app.js
@@ -1,48 +1,32 @@
 const express = require("express");
 const app = express();
-
 const bodyParser = require("body-parser");
-
 const dotenv = require("dotenv");
-
-const cors = require('cors');
-
 dotenv.config();
-
 const sequelize = require("./util/database");
-
 const userRouter = require("./router/userRouter");
 const expenseRouter = require("./router/expenseRouter");
 const purchaseMembershipRouter = require("./router/purchaseMembershipRouter");
 const leaderboardRouter = require("./router/leaderboardRouter");
+const resetPasswordRouter = require("./router/resetPasswordRouter");
 
 const User = require("./models/userModel");
 const Expense = require("./models/expenseModel");
 const Order = require("./models/ordersModel");
+const ResetPassword = require("./models/resetPasswordModel");
 
-app.use(cors());
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use("/", userRouter);
+app.use("/user", userRouter);
+app.use("/homePage", expenseRouter);
+app.use("/expense", expenseRouter);
+app.use("/purchase", purchaseMembershipRouter);
 
+app.use("/premium", leaderboardRouter);
 
-try {
-  app.use("/", userRouter);
-  app.use("/user", userRouter);
-  app.use("/homePage", expenseRouter);
-  app.use("/expense", expenseRouter);
-
-  app.use("/purchase", purchaseMembershipRouter);
-
-  app.use("/premium", leaderboardRouter);
-
-} catch (error) {
-  console.error('Error in setting up routers: ', error.message)
-}
-
-
-User.hasMany(Expense);
-Expense.belongsTo(User);
+app.use("/password", resetPasswordRouter);
 
 User.hasMany(Expense);
 Expense.belongsTo(User);
@@ -50,12 +34,12 @@ Expense.belongsTo(User);
 User.hasMany(Order);
 Order.belongsTo(User);
 
+ResetPassword.belongsTo(User);
+User.hasMany(ResetPassword);
 
 sequelize
   .sync()
   .then((result) => {
-    app.listen(3000, () => {
-      console.log("Server is running on port 3000");
-    });
+    app.listen(3000);
   })
   .catch((err) => console.log(err));
